52d0ecfd5c8a0182b218b17c5ecc907400ef3e56
RavinduSamaranayake <ovkravindu95@gmail.com>
2018-10-22 03:42:19 +0530

    Complete Order

--- app/src/main/java/com/example/user/lolascupcakes/placingOrder.java
+++ app/src/main/java/com/example/user/lolascupcakes/placingOrder.java
@@ -8,8 +8,11 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.user.lolascupcakes.Controlers.DatabaseHelper;
 import com.example.user.lolascupcakes.Controlers.DatePickerFragment;
+import com.example.user.lolascupcakes.Models.Order;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -20,6 +23,8 @@ public class placingOrder extends AppCompatActivity implements DatePickerDialog.
     TextView dateview;
     Button add_order;
     Button set_date;
+    DatabaseHelper dbase;
+    Order order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +34,35 @@ public class placingOrder extends AppCompatActivity implements DatePickerDialog.
         dateview =(TextView) findViewById(R.id.tvdate);
         add_order =(Button) findViewById(R.id.btnaddOrder);
         set_date =(Button) findViewById(R.id.btnsetDate);
+        dbase = new DatabaseHelper(this);
+        order = new Order();
         set_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerFragment datePicker = new DatePickerFragment();
                 datePicker.show(getSupportFragmentManager(),"date picker");
+            }
+        });
+        add_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (type.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Order Type is Empty", Toast.LENGTH_LONG).show();
+                } else if (qty.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Quantity is Empty", Toast.LENGTH_LONG).show(); }
+                else if (dateview.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "The date is required", Toast.LENGTH_LONG).show(); }
+
+                else{
+                    String tpe = type.getText().toString();
+                    String qt = qty.getText().toString();
+                    String date = dateview.getText().toString();
+                    order.setType(tpe);
+                    order.setQty(qt);
+                    order.setDate(date);
+                    dbase.createOrder(order);
+                    Toast.makeText(getApplicationContext(), "Your Placing Order is Success", Toast.LENGTH_LONG).show();
+                } 
             }
         });
 
